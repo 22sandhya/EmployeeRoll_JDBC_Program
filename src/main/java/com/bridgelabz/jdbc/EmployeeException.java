b3625fce6a700d06c23a21bc0d84cc1237fb14d6
Admin <sandhyabagale10@gmail.com>
2021-11-13 17:08:53 +0530

    Added:to perform all operation

--- src/main/java/com/bridgelabz/jdbc/EmployeeException.java
+++ src/main/java/com/bridgelabz/jdbc/EmployeeException.java
@@ -1,5 +1,11 @@
 package com.bridgelabz.jdbc;
 
+/**
+ * Purpose: This class is used for creating Costume Exception
+ * @author Sandhya
+ *
+ */
+
 public class EmployeeException extends RuntimeException {
 
 	public EmployeeException(String message) {
--- src/main/java/com/bridgelabz/jdbc/EmployeePayRollService.java
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayRollService.java
@@ -4,33 +4,42 @@ import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class EmployeePayRollService {
+
 	public ArrayList<Employee> empList;
+	// * created prepared statement
 	PreparedStatement preparedStatement;
+	// * Calling getConfig() to establish connection
 	Connection connection = EmployeeConfig.getConfig();
 
+	/**
+	 * created a generic type method which is expecting quaries type parameter
+	 * 
+	 * @param query
+	 * @return
+	 */
+
 	public List<Employee> queryExecute(String query) {
-		empList = new ArrayList<>();
+		empList = new ArrayList<Employee>();
 		try {
 			preparedStatement = connection.prepareStatement(query);
 			ResultSet resultSet = preparedStatement.executeQuery();
 			while (resultSet.next()) {
 				Employee employee = new Employee();
 
-				employee.setEmpId(resultSet.getInt("EmpId"));
-				employee.setEmpName(resultSet.getString("EmpName"));
-				employee.setPhoneNumber(resultSet.getString("PhoneNumber"));
+				employee.setID(resultSet.getInt("ID"));
+				employee.setName(resultSet.getString("Name"));
+				employee.setPhoneNumber(resultSet.getInt("PhoneNumber"));
 				employee.setAddress(resultSet.getString("Address"));
 				employee.setDepartment(resultSet.getString("Department"));
-				employee.setEmpStart(resultSet.getString("EmpStart"));
+				employee.setStart(resultSet.getString("Start"));
 				employee.setBasicPay(resultSet.getDouble("BasicPay"));
-				employee.setGender(resultSet.getString("Gender"));
 				employee.setDeductions(resultSet.getDouble("Deductions"));
 				employee.setTaxablePay(resultSet.getDouble("TaxablePay"));
 				employee.setIncomeTax(resultSet.getDouble("IncomeTax"));
 				employee.setNetPay(resultSet.getDouble("NetPay"));
-
 				empList.add(employee);
 			}
 		} catch (SQLException e) {
@@ -39,18 +48,29 @@ public class EmployeePayRollService {
 		return empList;
 	}
 
+	/**
+	 * this method is used to print records from the payroll_service table
+	 */
 	public void display() {
 		for (Employee i : empList) {
 			System.out.println(i.toString());
 		}
 	}
 
-	public double updateBasicPay(String empName, double basicPay) {
-		String UPDATE = "UPDATE employee_payroll SET BasicPay = ? WHERE EmpName = ?";
+	/**
+	 * this method is accepting two parameter and used for update the basic pay into
+	 * the salary column from the payroll_service table
+	 * 
+	 * @param empName
+	 * @param basicPay
+	 * @return
+	 */
+	public double updateBasicPay(String Name, double basicPay) {
+		String UPDATE = "UPDATE employee_payroll SET BasicPay = ? WHERE Name = ?";
 		try {
 			preparedStatement = connection.prepareStatement(UPDATE);
 			preparedStatement.setDouble(1, basicPay);
-			preparedStatement.setString(2, empName);
+			preparedStatement.setString(2, Name);
 			preparedStatement.executeUpdate();
 			System.out.println("update successfully");
 		} catch (SQLException e) {
@@ -59,14 +79,21 @@ public class EmployeePayRollService {
 		String sql = "SELECT * FROM employee_payroll";
 		queryExecute(sql);
 		for (Employee employee : empList) {
-			if (employee.getEmpName().equals(empName)) {
+			if (employee.getName().equals(Name)) {
 				return employee.getBasicPay();
 			}
 		}
 		return 0.0;
 	}
 
-	public void selectEmployee(LocalDate start, LocalDate end) {
+	/**
+	 * this is a parameterized method which is used ti print the employee from a
+	 * range of given date
+	 * 
+	 * @param start
+	 * @param end
+	 */
+	public void getEmployee(LocalDate start, LocalDate end) {
 		ArrayList<Employee> empSelected = new ArrayList<>();
 		String select = "SELECT * FROM employee_payroll WHERE EmpStart BETWEEN ? AND ?";
 		String sDate = String.valueOf(start);
@@ -79,14 +106,13 @@ public class EmployeePayRollService {
 			while (resultSet.next()) {
 				Employee employee = new Employee();
 
-				employee.setEmpId(resultSet.getInt("EmpId"));
-				employee.setEmpName(resultSet.getString("EmpName"));
-				employee.setPhoneNumber(resultSet.getString("PhoneNumber"));
+				employee.setID(resultSet.getInt("ID"));
+				employee.setName(resultSet.getString("Name"));
+				employee.setPhoneNumber(resultSet.getInt("PhoneNumber"));
 				employee.setAddress(resultSet.getString("Address"));
 				employee.setDepartment(resultSet.getString("Department"));
-				employee.setEmpStart(resultSet.getString("EmpStart"));
+				employee.setStart(resultSet.getString("Start"));
 				employee.setBasicPay(resultSet.getDouble("BasicPay"));
-				employee.setGender(resultSet.getString("Gender"));
 				employee.setDeductions(resultSet.getDouble("Deductions"));
 				employee.setTaxablePay(resultSet.getDouble("TaxablePay"));
 				employee.setIncomeTax(resultSet.getDouble("IncomeTax"));
@@ -100,6 +126,75 @@ public class EmployeePayRollService {
 
 		} catch (SQLException e) {
 			throw new EmployeeException("Invalid date");
+		}
+	}
+
+	/**
+	 * This method is used to find sum, average, min, max from the payroll_service
+	 * table
+	 * 
+	 */
+
+	public void calculate() {
+		Scanner scanner = new Scanner(System.in);
+
+		final int EXIT = 6;
+		int choice = 0;
+		while (choice != EXIT) {
+			System.out.println("enter your choice\n1. SUM\n2. AVG\n3. MIN\n4. MAX  \n5.COUNT\n6.EXIT\n");
+			choice = scanner.nextInt();
+			switch (choice) {
+			case 1:
+				calculateQuery("SELECT Gender, SUM(BasicPay) FROM employee_payroll GROUP BY Gender");
+				break;
+
+			case 2:
+				calculateQuery("SELECT Gender, AVG(BasicPay) FROM employee_payroll GROUP BY Gender");
+				break;
+
+			case 3:
+				calculateQuery("SELECT Gender, MIN(BasicPay) FROM employee_payroll GROUP BY Gender");
+				break;
+			case 4:
+				calculateQuery("SELECT Gender, MAX(BasicPay) FROM employee_payroll GROUP BY Gender");
+				break;
+			case 5:
+				calculateQuery("SELECT Gender, COUNT(BasicPay) FROM employee_payroll GROUP BY Gender");
+				break;
+			}
+		}
+	}
+
+	/**
+	 * this method is used to print the basic pay by using the gender
+	 * 
+	 * @param calculate
+	 */
+
+	public void calculateQuery(String calculate) {
+		List<Employee> result = new ArrayList<Employee>();
+
+		try {
+			preparedStatement = connection.prepareStatement(calculate);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				Employee employee = new Employee();
+				employee.setGender(resultSet.getString(1));
+				employee.setBasicPay(resultSet.getDouble(2));
+
+				result.add(employee);
+			}
+			if (calculate.contains("COUNT")) {
+				for (Employee i : result) {
+					System.out.println("Gender: " + i.getGender() + " COUNT: " + i.getBasicPay());
+				}
+			} else {
+				for (Employee i : result) {
+					System.out.println("Gender: " + i.getGender() + " Basic pay: " + i.getBasicPay());
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
